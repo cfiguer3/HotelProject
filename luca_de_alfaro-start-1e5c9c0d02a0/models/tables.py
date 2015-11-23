b527975d8ac0dd5eb7ceb9d6166b474642d17b02
cfiguer3 <cfiguer3@ucsc.edu>
2015-11-22 17:06:39 -0800

    lecture15

---
+++ luca_de_alfaro-start-1e5c9c0d02a0/models/tables.py
@@ -0,0 +1,23 @@
+#########################################################################
+## Define your tables below; for example
+##
+## >>> db.define_table('mytable',Field('myfield','string'))
+##
+## Fields can be 'string','text','password','integer','double','boolean'
+##       'date','time','datetime','blob','upload', 'reference TABLENAME'
+## There is an implicit 'id integer autoincrement' field
+## Consult manual for more options, validators, etc.
+##
+## More API examples for controllers:
+##
+## >>> db.mytable.insert(myfield='value')
+## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
+## >>> for row in rows: print row.id, row.myfield
+#########################################################################
+
+db.define_table('post',
+             Field('author', db.auth_user, default=auth.user_id),
+             Field('message_content', 'text'),
+             Field('is_draft', 'boolean', default=False),
+             Field('message_id'), # To uniquely identify drafts and messages.
+            )
